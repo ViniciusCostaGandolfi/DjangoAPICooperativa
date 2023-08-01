70285ffe71e3cb3ec45570c057ac0564f1dd0005
Vinicius Costa Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2023-08-01 16:03:20 -0300

    Update urls.py

--- cooperative/urls.py
+++ cooperative/urls.py
@@ -28,14 +28,6 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    # path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router.urls))
 
 ]
-# rotafood.railway.app/logistic
-# rotafood.railway.app/orders
-# rotafood.railway.app/products
-# rotafood.railway.app/deliveryman
-# rotafood.railway.app/registrar-usuario/
